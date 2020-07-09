7d6c25fccd2451a087120c5fc245fbb97acd91b8
gido5731 <celtictarot386@gmail.com>
2020-07-09 14:07:24 -0400

    downloads the thing and spits out len

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gidoBOSSftw5731/Historical-ROA
 
 go 1.13
+
+require github.com/gidoBOSSftw5731/log v0.0.0-20190718204308-3ae037c6203f
--- main.go
+++ main.go
@@ -1,5 +1,110 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/gidoBOSSftw5731/log"
+)
+
+// inputROA is a Struct with all the data from the json
+// we do NOT store this directly.
+// https://mholt.github.io/json-to-go/
+type inputROA struct {
+	Asn       string `json:"asn"`
+	Prefix    string `json:"prefix"`
+	MaxLength int    `json:"maxLength"`
+	Ta        string `json:"ta"`
+}
+
+type inputROAArr struct {
+	Roas []inputROA `json:"roas"`
+}
+
+// storedROAs is what we store, we simply trim the subnet
+// from the input ROA and store it seperately.
+type storedROA struct {
+	Asn       string `json:"asn"`
+	Prefix    string `json:"prefix"`
+	MaxLength int    `json:"maxLength"`
+	Ta        string `json:"ta"`
+	Subnet    int
+}
+
+const (
+	roaURL = "https://hosted-routinator.rarc.net/json"
+)
+
+func main() {
+	log.SetCallDepth(4)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+		log.Tracef("using default port: %v", port)
+	}
+
+	http.HandleFunc("/update", pullToDB)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func pullToDB(w http.ResponseWriter, r *http.Request) {
+	origIn, err := downloadRARC()
+	if err != nil {
+		ErrorHandler(w, r, 500, "Error parsing JSON", err)
+		return
+	}
+
+	var in []storedROA
+	for _, i := range origIn.Roas {
+		// shut up I know its not correct terminology
+		ipandmask := strings.Split(i.Prefix, "/")
+		// probably doesnt need error checking
+		mask, _ := strconv.Atoi(ipandmask[1])
+
+		in = append(in, storedROA{
+			Asn:       i.Asn,
+			Prefix:    ipandmask[0],
+			MaxLength: i.MaxLength,
+			Ta:        i.Ta,
+			Subnet:    mask,
+		})
+	}
+
+	log.Traceln(len(in))
+}
+
+func downloadRARC() (*inputROAArr, error) {
+	var form inputROAArr
+
+	resp, err := http.Get(roaURL)
+	if err != nil {
+		return &form, err
+	}
+	defer resp.Body.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	jsonIn := buf.String()
+
+	err = json.Unmarshal([]byte(jsonIn), &form)
+	if err != nil {
+		return &form, err
+	}
+
+	return &form, nil
+}
+
+//ErrorHandler is a function to handle HTTP errors
+//copied from imgsrvr, slightly different formatting
+func ErrorHandler(resp http.ResponseWriter, req *http.Request, status int, alert string, err error) {
+	log.Errorln(err)
+	resp.WriteHeader(status)
+	log.Error("artifical http error: ", status)
+	fmt.Fprintf(resp, "You have found an error! This error is of type %v. Built in alert: \n'%v',\n Would you like a <a href='https://http.cat/%v'>cat</a> or a <a href='https://httpstatusdogs.com/%v'>dog?</a>",
+		status, alert, status, status)
 }
